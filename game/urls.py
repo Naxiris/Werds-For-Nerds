b3fd2bd9dfb86249f22d65b1bed062dc5b0d09ef
Nadia Bakal <gbqlaced@gmail.com>
2025-03-27 17:25:41 +0000

    Add redirect from homepage to leaderboard

--- game/urls.py
+++ game/urls.py
@@ -1,8 +1,10 @@
+from django.shortcuts import redirect
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('', lambda request: redirect('leaderboard')),
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='game/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='game/logged_out.html'), name='logout'),
