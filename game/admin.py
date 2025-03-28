b322724a773e205cb5158d00a5fb333f3f6481be
Nadia Bakal <gbqlaced@gmail.com>
2025-03-28 13:09:39 +0000

    Add profile link to navbar and fix layout

--- game/admin.py
+++ game/admin.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
 
-admin.site.unregister(User) 
+# Unregister the default User admin
+admin.site.unregister(User)
 
+# Re-register User with custom admin settings
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
     list_display = ('username', 'email', 'is_staff', 'is_superuser', 'last_login')
     search_fields = ('username', 'email')
     list_filter = ('is_staff', 'is_superuser')
+
